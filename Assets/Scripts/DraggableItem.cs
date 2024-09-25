47ab9ee911ac4438ac38b6a2b6e0b88f92db50fb
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-25 15:20:35 +0200

    feat : drag and drop inventory system

---
+++ Assets/Scripts/DraggableItem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [HideInInspector]public Transform parentToReturnTo = null;
+    public Image image;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        parentToReturnTo = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        transform.SetParent(parentToReturnTo);
+        image.raycastTarget = true;
+    }
+
+}
