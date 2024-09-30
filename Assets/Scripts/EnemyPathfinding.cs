e82962206cd93332fe5d23bff65268bbc1ebdf40
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-09-30 17:04:47 +0200

    feat : new invisible enclosure to stop mobs from roaming everywhere

--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -20,4 +20,23 @@ public class EnemyPathfinding : MonoBehaviour
     public void MoveTo(Vector2 targetPosition) {
         moveDir = targetPosition;
     }
+
+    // Handle collision with the MonsterBlock layer
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("MonsterBlock")) {
+            // The monster collided with an invisible enclosure
+            // Here, we stop the monster's movement or make it turn around
+            StopMovingOrChangeDirection();
+        }
+    }
+
+    // Stop the movement or change the direction of the monster
+    private void StopMovingOrChangeDirection() {
+        // To stop the monster, set the move direction to zero
+        moveDir = Vector2.zero;
+
+        // Alternatively, to change direction, generate a new roam position:
+        Vector2 newDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        MoveTo(newDirection); // Change direction to roam somewhere else
+    }
 }
