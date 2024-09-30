c5f3693aa315400f11ac148ac2731ef0344ce498
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-30 11:13:39 +0200

    feat : changed prefabs of players

--- Assets/Scripts/BushStomping.cs
+++ Assets/Scripts/BushStomping.cs
@@ -1,23 +1,40 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-[RequireComponent(typeof(SpriteRenderer))]
-public sealed class BushStomping : MonoBehaviour
+[RequireComponent(typeof(Tilemap))]
+public class BushStomping : MonoBehaviour
 {
-  #region Inspector
+    #region Inspector
 
-  [SerializeField]
-  private Sprite stompSprite = default;
+    [SerializeField] private TileBase stompedBushTile; // Tile to replace the bush when stomped
+    [SerializeField] private Tilemap bushTilemap;      // Reference to the Tilemap with bushes
 
-  #endregion
+    #endregion
 
+    #region MonoBehaviour
 
-  #region MonoBehaviour
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the bush"); 
+            // Get the position of the collision in world space
+            Vector3 playerPosition = other.transform.position;
 
-  private void OnTriggerEnter2D (Collider2D other)
-  {
-    if ( other.CompareTag("Player") )
-      GetComponent<SpriteRenderer>().sprite = stompSprite;
-  }
+            // Convert the player's world position to the tilemap's grid position
+            Vector3Int tilePosition = bushTilemap.WorldToCell(playerPosition);
 
-  #endregion
+            // Get the tile at the player's position
+            TileBase currentTile = bushTilemap.GetTile(tilePosition);
+
+            // If the current tile is a bush (you can add further checks here)
+            if (currentTile != null) // Check if a tile exists at this position
+            {
+                // Replace the tile with the stomped version
+                bushTilemap.SetTile(tilePosition, stompedBushTile);
+            }
+        }
+    }
+
+    #endregion
 }
