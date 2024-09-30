eb7a490443b1eb604a336a80e2c098254c853506
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-30 16:29:15 +0200

    feat : class to manage the player slots

---
+++ Assets/Scripts/PlayerSlot.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerSlot : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private RectTransform iconArea; // The UI area where the prefab will be instantiated
+    private GameObject instantiatedPrefab;
+    private Player player;
+
+    // Method to assign the player prefab and instantiate it in the UI
+    public void SetPlayerPrefab(GameObject playerPrefab, Player playerData)
+    {
+        // Clear the previous prefab
+        if (instantiatedPrefab != null)
+        {
+            Destroy(instantiatedPrefab);
+        }        
+        player = playerData;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.clickCount == 2)
+        {
+            PlayerSelectionManager.Instance.ShowPlayerStats(player); // Notify manager on double-click
+        }
+    }
+}
