54726e17a912e8966e2a8e0dabea4639fc2349b1
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-10-01 11:22:07 +0200

    feat : add different color to selected slot (not working yet)

--- Assets/Scripts/PlayerSlot.cs
+++ Assets/Scripts/PlayerSlot.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayerSlot : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private RectTransform iconArea; // The UI area where the prefab will be instantiated
     private GameObject instantiatedPrefab;
-    private Player player;
+    private Player player;  // Store the Player data
+    public Image image;
+    public Color selectedColor, notSelectedColor;
 
-    // You can add a public property for the slot name
-    public string SlotName => gameObject.name; // Returns the name of the GameObject
 
     // Method to assign the player prefab and instantiate it in the UI
     public void SetPlayerPrefab(GameObject playerPrefab, Player playerData)
@@ -17,15 +18,19 @@ public class PlayerSlot : MonoBehaviour, IPointerClickHandler
         if (instantiatedPrefab != null)
         {
             Destroy(instantiatedPrefab);
-        }        
-        player = playerData;
+        }
+
+        instantiatedPrefab = playerPrefab;
+        player = playerData;  // Store the player data for this slot
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.clickCount == 2)
         {
-            PlayerSelectionManager.Instance.ShowPlayerStats(player); // Notify manager on double-click
+            // Notify manager on double-click and show player stats
+            PlayerSelectionManager.Instance.ShowPlayerStats(player);
+            image.color = selectedColor;
         }
     }
 }
