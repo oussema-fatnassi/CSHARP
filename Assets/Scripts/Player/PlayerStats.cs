523c01df66f86635ac6c94b5665968df4b00fe8b
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-10-09 11:18:46 +0200

    feat : add maxHealth and maxMana

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -5,7 +5,9 @@ public class PlayerStats : ScriptableObject
 {
     public string playerName;
     public int health;
+    public int maxHealth;
     public int mana;
+    public int maxMana;
     public int damage;
     public int defense;
     public int speed;
