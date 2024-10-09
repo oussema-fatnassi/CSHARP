da8701466d9438247e1748108d15d63cf514c358
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-10-09 17:01:14 +0200

    feat : add camera bounds to camera after warping to new location

---
+++ Assets/Scripts/Other/CameraManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance;
+
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    private CinemachineConfiner confiner;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        confiner = virtualCamera.GetComponent<CinemachineConfiner>();
+    }
+
+    public void SetCameraBounds(Collider2D bounds)
+    {
+        if (confiner != null)
+        {
+            confiner.m_BoundingShape2D = bounds;
+            confiner.InvalidatePathCache(); // Refresh the confiner to apply the new bounds immediately
+        }
+    }
+}
--- Assets/Scripts/Other/Warp.cs
+++ Assets/Scripts/Other/Warp.cs
@@ -6,12 +6,17 @@ public class Warp : MonoBehaviour
 {
     [SerializeField] private Transform destination;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private Collider2D newCameraBounds; // Set this to the new bounds for this warp area
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (playerLayer == (playerLayer | (1 << collision.gameObject.layer)))
         {
+            // Warp the player
             collision.transform.position = destination.position;
+
+            // Update the camera bounds
+            CameraManager.Instance.SetCameraBounds(newCameraBounds);
         }
     }
 }
