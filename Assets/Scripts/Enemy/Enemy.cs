16c7971d0135ac0de30f1c8dd85c864a14a74355
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-11-12 09:38:27 +0100

    chore : add comments

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    This script is the base class for all enemies in the game.
+    It contains the stats of the enemy and a method to initialize the enemy with new stats.
+*/
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] protected EnemyStats stats;
 
+    // Properties
     public string EnemyName { get => stats.enemyName; }
     public int Health { get => stats.health; set => stats.health = value; }
     public int Damage { get => stats.damage; set => stats.damage = value; }
@@ -15,6 +21,7 @@ public class Enemy : MonoBehaviour
     public int ExperienceToGive { get => stats.experienceToGive; set => stats.experienceToGive = value; }
     public int Level { get => stats.level; set => stats.level = value; }
 
+    // Initialize the enemy with new stats
     public void InitializeEnemy(EnemyStats newStats)
     {
         stats = newStats;
