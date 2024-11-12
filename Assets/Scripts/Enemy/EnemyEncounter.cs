5bb8c3e8338c04f47557f1d2b974a2b67832aa69
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-11-12 09:38:34 +0100

    chore : add comments

--- Assets/Scripts/Enemy/EnemyEncounter.cs
+++ Assets/Scripts/Enemy/EnemyEncounter.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    This script is attached to the EnemyEncounter prefab.
+    It is used to detect when the player enters the enemy's range and triggers a battle.
+    It also destroys the enemy encounter object after a delay.
+*/
+
 public class EnemyEncounter : MonoBehaviour
 {
+    [SerializeField] GameObject BattleSystemObject;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
