6ac69da52f314a61c297b112d2c53b0d88be66d5
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-11-12 09:38:04 +0100

    chore : add comments

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+    This class is responsible for setting the dialog text in the battle dialog box.
+*/
+
 public class BattleDialogBox : MonoBehaviour
 {
     [SerializeField] private Text dialogText;
 
+    // Set the dialog text
     public void SetDialog(string dialog) {
         dialogText.text = dialog;
     }
