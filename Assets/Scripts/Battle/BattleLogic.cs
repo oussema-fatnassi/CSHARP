bd27ceecddeef4a2c5c2af5f09966c5a60a313d4
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-11-12 09:38:11 +0100

    chore : add comments

--- Assets/Scripts/Battle/BattleLogic.cs
+++ Assets/Scripts/Battle/BattleLogic.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    This class will be responsible for the logic of the battle system.
+    It will handle the turns, the actions, the damage, the healing, etc.
+*/
+
 public class BattleLogic : MonoBehaviour
 {
     // Start is called before the first frame update
