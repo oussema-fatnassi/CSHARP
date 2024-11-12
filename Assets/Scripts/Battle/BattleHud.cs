0ca8463ec1b3318fbef38cfc53b1409a62fa9a6b
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-11-12 09:38:08 +0100

    chore : add comments

--- Assets/Scripts/Battle/BattleHud.cs
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+    This class is responsible for setting the data of the player in the battle.
+    It sets the player's name, level, health, and mana.
+*/
+
 public class BattleHud : MonoBehaviour
 {
 
@@ -12,6 +17,7 @@ public class BattleHud : MonoBehaviour
     [SerializeField] ManaBar manaBar;
     // [SerializeField] BattleDialogBox dialogBox;
 
+    // Set the player's name, level, health, and mana.
     public void SetData(PlayerStats player)
     {
         nameText.text = player.playerName;
